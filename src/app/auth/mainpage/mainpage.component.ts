9dddb5255126ebc356afd7b9538fc6569458c203
Astha66 <Astha.Sharma@iiitb.org>
2020-04-12 18:01:03 +0530

    added translation

--- src/app/auth/mainpage/mainpage.component.ts
+++ src/app/auth/mainpage/mainpage.component.ts
@@ -1,5 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'events';
 
+
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { MatDialog } from '@angular/material';
+import { DialougComponent } from 'src/app/shared/dialoug/dialoug.component';
+import { Subscription, range } from 'rxjs';
+import LanguageFactory from 'src/assets/i18n';
+import { DataStorageService } from 'src/app/core/services/data-storage.service';
+import { ConfigService } from 'src/app/core/services/config.service';
+import * as appConstants from '../../app.constants';
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
@@ -7,9 +19,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainpageComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
+
+generateVid()
+{
+ // this.router.
+}
 
 }
